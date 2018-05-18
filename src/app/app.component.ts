34de274de887b00c7b1d179dc16d0f829f5a16fa
unknown <adrian.madalin.dinu@gmail.com>
2018-05-19 02:22:11 +0300

    updated angular to v6.0.2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import {Component} from "@angular/core";
+import {HttpModule} from "@angular/http";
+import {HttpClient, HttpClientModule, HttpHeaderResponse, HttpHeaders} from "@angular/common/http";
 
 @Component({
     selector: 'app-root',
@@ -8,4 +10,18 @@ import {Component} from "@angular/core";
 
 export class AppComponent {
 
+    constructor(private _http: HttpClient) {
+
+    }
+
+    onClick() {
+        const headers = new HttpHeaders();
+        headers.append('Content-type', 'application/json')
+        this._http.get('https://jsonplaceholder.typicode.com/users', {headers}).subscribe(
+            (res) => {
+                console.log(res);
+            },
+            (err) => console.log(`${err}`)
+        );
+    }
 }
